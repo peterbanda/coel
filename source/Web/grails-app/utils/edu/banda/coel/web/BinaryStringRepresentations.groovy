92d322dfc54c3a3080c6bf35f0d654494d1bbf45
peterbanda <tremor11@gmail.com>
2015-06-18 01:10:05 -0700

    A lot of usability and UI improvements (help, 'start here', transition table bin/dec/hex import, etc.)

--- source/Web/grails-app/utils/edu/banda/coel/web/BinaryStringRepresentations.groovy
+++ source/Web/grails-app/utils/edu/banda/coel/web/BinaryStringRepresentations.groovy
@@ -4,6 +4,7 @@ package edu.banda.coel.web
  * Created by peter on 6/17/15.
  */
 class BinaryStringRepresentations {
+
     String binaryString
     String decimalString
     String hexadecimalString
